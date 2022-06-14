56a48a9616f93175e6e8915786bbb3ff6244d13a
denfort50 <denfort50@yandex.ru>
2022-06-14 07:07:46 +0300

    3. Прерывание нити [#1019]

---
+++ src/test/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        String[] process = new String[] {"-", "\\", "|", "/"};
+        int counter = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\rLoading: " + process[counter]);
+                counter++;
+                if (counter == 4) {
+                    counter = 0;
+                }
+                Thread.sleep(500);
+            }
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+
+
+}
