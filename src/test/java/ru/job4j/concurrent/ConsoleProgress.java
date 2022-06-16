cd0c935034db92c0382b510f2ececc108628ab89
denfort50 <denfort50@yandex.ru>
2022-06-16 05:44:55 +0300

    3.1. Прерывание блокированной нити. [#267413]

--- src/test/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/test/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -17,7 +17,7 @@ public class ConsoleProgress implements Runnable {
             }
 
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            Thread.currentThread().interrupt();
         }
     }
 
