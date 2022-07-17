d3eebbc45de4bc5d3d8672cf2319fb10c7d13f40
denfort50 <denfort50@yandex.ru>
2022-07-17 09:42:33 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/mail/EmailNotification.java
@@ -0,0 +1,36 @@
+package ru.job4j.mail;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
+        String body = String.format("Add a new event to %s", user.getUsername());
+        pool.submit(() -> send(subject, body, user.getEmail()));
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        EmailNotification emailNotification = new EmailNotification();
+        User patrick = new User("Patrick", "patrick777@gmail.com");
+        emailNotification.emailTo(patrick);
+    }
+}
